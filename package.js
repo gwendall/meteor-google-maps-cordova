7a0137ced8731abbd9201c3f24d54165f23d144f
Gwen <gwendall.esnault@gmail.com>
2015-10-08 10:27:15 +0200

    first

--- package.js
+++ package.js
@@ -1,11 +1,10 @@
 Package.describe({
   name: 'gwendall:google-maps-cordova',
   summary: 'Native Google Maps for Cordova',
-  version: '0.0.1'
+  version: '0.1.0'
 });
 
 Cordova.depends({
-  // 'plugin.google.maps': '1.2.4',
   'plugin.google.maps': 'https://github.com/wf9a5m75/phonegap-googlemaps-plugin/tarball/e77d0edc6e561c74dc9ce755f93ee5295e74185f'
 });
 
@@ -13,6 +12,5 @@ Package.onUse(function(api) {
   api.versionsFrom('1.0');
   api.use(['reactive-var','templating','tracker', 'underscore'], 'client');
   api.addFiles(['api.js'], 'client');
-  api.addFiles(['map.html','map.js'],'client');
   api.export('MapControl');
 });
