c8f152b826bd0fd533397a0d8570e294dd54ee21
Gwen <gwendall.esnault@gmail.com>
2015-10-08 10:30:26 +0200

    first

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
   name: 'gwendall:google-maps-cordova',
   summary: 'Native Google Maps for Cordova',
-  git: 'https://github.com/gwendall/meteor-google-maps-cordova.git'
+  git: 'https://github.com/gwendall/meteor-google-maps-cordova.git',
   version: '0.1.1'
 });
 
